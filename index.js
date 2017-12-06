8f1be97f9ad09b69d90a2528234cc92fa9dd937c
Ian Egner <ian.egner@apc-overnight.com>
2017-12-06 09:05:30 +0000

    Show config parse error nicely and exit process

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const Hue = require('./src/Hue')
 
 const startHue = async () => {
   // Load config from file
-  const config = require('rc')('hue', require('./lib/default-config'))
-  const hue = new Hue(config)
+  try {
+    const config = require('rc')('hue', require('./lib/default-config'))
 
-  await hue.init()
-  await hue.start()
+    const hue = new Hue(config)
+
+    await hue.init()
+    await hue.start()
+  } catch (e) {
+    console.error('Error loading Config File')
+    console.error(e.message)
+    console.info('Add a .huerc config file - See https://www.npmjs.com/package/huestatus for more info')
+    process.exit(1)
+  }
 }
 
 module.exports = startHue()
